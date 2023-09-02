272e74e90785c2bed70764546605caf6695fcb63
Nilesh Sheoran <nksheoran2002@gmail.com>
2023-09-02 14:06:36 +0530

    show teacher feature implemented

---
+++ lib/teacher/ui/show_teacher_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShowTeacherScreen extends StatefulWidget {
+  const ShowTeacherScreen({super.key});
+
+  @override
+  State<ShowTeacherScreen> createState() => _ShowTeacherScreenState();
+}
+
+class _ShowTeacherScreenState extends State<ShowTeacherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
